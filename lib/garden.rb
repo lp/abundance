df36b53ab45dd0e99c7dc75a5f2a620cf313d81e
lp <lp@spiralix.org>
2009-01-13 00:17:27 -0500

    roughly externalized most garden code inside the main loop

--- lib/garden.rb
+++ lib/garden.rb
@@ -38,141 +38,30 @@ class Garden
       @seeds = []; @sprouts = []; @crops = []; @id = 0
       set_my_socket_as_a(:garden)
       loop do
-        catch :fill_rows do
-          loop do
-            if ! @seed_all.nil? && ! @rows_socket_paths.empty? && @seed_all_done.size != @seed_all[0]
-              row_socket_path = @rows_socket_paths.shift
-              unless @seed_all_done.include?( row_socket_path )
-                socket_send(:seed_all,@seed_all[1],row_socket_path)
-                @seed_all_done << row_socket_path
-              else
-                @rows_socket_paths << row_socket_path
-              end
-            elsif ! @do_init.nil? && ! @rows_socket_paths.empty? && @init_done.size != @do_init
-              row_socket_path = @rows_socket_paths.shift
-              unless @init_done.include?( row_socket_path )
-                socket_send(:init,'init_status',row_socket_path)
-                @init_done << row_socket_path
-              else
-                @rows_socket_paths << row_socket_path
-              end
-            elsif ! @seeds.empty? && ! @rows_socket_paths.empty?
-              seed = @seeds.shift
-              @sprouts[seed[:id]] = seed
-              row_socket_path = @rows_socket_paths.shift
-              socket_send(:sprout,seed,row_socket_path)
-            elsif @quit && ! @rows_socket_paths.empty?
-              seed = nil
-              row_socket_path = @rows_socket_paths.shift
-              socket_send(:quit,seed,row_socket_path)
-            else
-              throw :fill_rows
-            end               
-          end
-        end
+        seed_if_row_available
+        
         command, data, client_socket_path = socket_recv
         case command
         when :seed
-          @id += 1; @seeds << {:id => @id , :seed => data}
-          socket_send(command,@id,client_socket_path)
+          place_seed_in_queue(command,data,client_socket_path)
         when :row
-          if @quit
-            command = :quit
-            seed = nil
-          elsif @seeds.empty?
-            seed = nil
-            @rows_socket_paths << data
-          else
-            seed = @seeds.shift
-            @sprouts[seed[:id]] = seed
-          end
-          socket_send(command,seed,client_socket_path)  
+          this_row_is_available(command,data,client_socket_path)  
         when :crop
-          @sprouts[data[:id]] = nil
-          @crops[data[:id]] = data
-          if @harvest[data[:id]]
-            socket_send(command,data, @harvest[data[:id]][:client_socket_path]) 
-            @crops[data[:id]] = @harvest[data[:id]] = nil
-          elsif @full_crop && @seeds.compact.empty? && @sprouts.compact.empty?
-            socket_send(command,@crops.compact,@mem_client_socket_path)
-            @crops.clear; @full_crop = false
-          end
+          save_crop_for(command,data,client_socket_path)
         when :growth
-          case data
-          when :progress
-            value = @crops.size.to_f / (@crops.size + @sprouts.compact.size + @seeds.size)
-            value = 1 if value.nan?; progress = sprintf( "%.2f", value)
-            socket_send(command,progress,client_socket_path)
-          when :seed
-            socket_send(command,@seeds.size,client_socket_path)
-          when :sprout
-            socket_send(command,@sprouts.compact.size,client_socket_path)
-          when :crop
-            socket_send(command,@crops.size,client_socket_path)
-          else
-            socket_send(command,false,client_socket_path)
-          end
+          report_growth(command,data,client_socket_path)
         when :harvest
-          case data
-          when :all
-            socket_send(command,{:seeds => @seeds, :sprouts => @sprouts.compact, :crops => @crops.compact},client_socket_path)
-          when :seed
-            socket_send(command,@seeds,client_socket_path)
-          when :sprout
-            socket_send(command,@sprouts.compact,client_socket_path)
-          when :crop
-            socket_send(command,@crops.compact,client_socket_path)
-            @crops.clear
-          when :full_crop
-            if @seeds.compact.empty? && @sprouts.compact.empty?
-              socket_send(command,@crops.compact,client_socket_path)
-              @crops.clear
-            else
-              @full_crop = true
-              @mem_client_socket_path = client_socket_path
-            end
-          else
-            if data.is_a? Integer
-              if @crops[data]
-                socket_send(command,@crops[data],client_socket_path)
-                @crops[data] = nil
-              else
-                @harvest[data] = {:client_socket_path => client_socket_path}
-              end
-            else
-              socket_send(command,false,client_socket_path)
-            end
-          end
+          harvest_some(command,data,client_socket_path)
         when :init
-          @do_init = data
-          @init_return = {:client_socket_path => client_socket_path}
+          ask_for_init_status(command,data,client_socket_path)
         when :init_crop
-          @init_all_crop << data
-          if @init_all_crop.size == @do_init
-            socket_send(command,@init_all_crop, @init_return[:client_socket_path])
-            @init_return = Hash.new; @init_done = Array.new; @do_init = nil; @init_all_crop = Array.new
-          end
+          answer_init_status(command,data,client_socket_path)
         when :seed_all
-          @seed_all = data
-          @seed_all_return = {:client_socket_path => client_socket_path, :data => []}
+          seed_for_all_rows(command,data,client_socket_path)
         when :seed_all_crop
-          @seed_all_crop << data
-          if @seed_all_crop.size == @seed_all[0]
-            socket_send(command,@seed_all_crop, @seed_all_return[:client_socket_path])
-            @seed_all = nil; @seed_all_return = Hash.new; @seed_all_done = Array.new; @seed_all_crop = Array.new
-          end
+          special_crop_seed_all(command,data,client_socket_path)
         when :close
-          if data[:level] == :garden
-            @seeds_pid = data[:pid]
-            @quit = true
-            @mem_client_socket_path = client_socket_path
-          else
-            @seeds_pid.delete(data[:pid].to_i)
-            if @seeds_pid.empty?
-              socket_send(:close,{:seeds => @seeds, :sprouts => @sprouts.compact, :crops => @crops.compact}, @mem_client_socket_path)
-              exit
-            end
-          end
+          close_all(command,data,client_socket_path)
         else
           socket_send(command,false,client_socket_path)
         end
