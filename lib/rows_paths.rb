70807f511dd8c2c920ef415087e97b4f10b12b41
lp <lp@spiralix.org>
2009-01-17 20:22:48 -0500

    new Garden::Rows::Paths module

---
+++ lib/rows_paths.rb
@@ -0,0 +1,13 @@
+class Garden
+  class Rows
+    # Author:: lp (mailto:lp@spiralix.org)
+    # Copyright:: 2008 Louis-Philippe Perron - Released under the terms of the MIT license
+    # :title:Rows
+    module Paths
+      
+      
+      
+      
+    end
+  end
+end
