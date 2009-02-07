45e0281596ad11186a4dca807a5540653cb5cd48
lp <lp@spiralix.org>
2009-02-06 19:27:40 -0500

    queue empty and finished test

---
+++ test/tc_queue.rb
@@ -0,0 +1,54 @@
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'test/unit'
+require 'abundance'
+
+class TestQueue < Test::Unit::TestCase
+	
+	def setup
+		@rows = 4
+		set_gardener	
+	end
+	
+	def test_queue
+		check_empty(true)
+		check_finished(true)
+		seed_lots
+		check_empty(false)
+		check_finished(false)
+		@g.harvest(:full_crop)
+		check_empty(true)
+		check_finished(true)
+	end
+	
+	def teardown
+		@g.close
+	end
+	
+	private
+	
+	def set_gardener
+    @g = Abundance.gardener(:rows => @rows, :init_timeout => 3) do
+      Abundance.init_status(true,Process.pid)
+      Abundance.grow do |seed|
+				sprout = seed.sprout
+				Array.new(100000,01).join
+        seed.crop(true, "gardener1: #{sprout}")
+      end      
+    end
+  end
+
+	def seed_lots
+		20.times do |num|
+			@g.seed(num.to_s)
+		end
+	end
+	
+	def check_empty(bool)
+		assert_equal(bool,@g.growth(:empty),"growth :empty does not return #{bool.to_s}")
+	end
+	
+	def check_finished(bool)
+		assert_equal(bool,@g.growth(:finished),"growth :finished does not return #{bool.to_s}")
+	end
+	
+end
