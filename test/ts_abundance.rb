45626d0899c1470e33e309988e7133e9aa1edc79
lp <lp@spiralix.org>
2009-02-06 19:28:43 -0500

    require tc_queue

--- test/ts_abundance.rb
+++ test/ts_abundance.rb
@@ -1,6 +1,8 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'test/unit'
+
 require 'test/tc_high_api'
 require 'test/tc_robustness'
 require 'test/tc_multi_gardener'
 require 'test/tc_burst'
+require 'test/tc_queue'
