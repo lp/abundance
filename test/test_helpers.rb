38d7d335b44de9e8175473df835f43f7ac78b923
lp <lp@spiralix.org>
2009-02-10 23:02:38 -0500

    good start of new gem structure here

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -1,66 +1,69 @@
-require 'optparse'
-class Arguments < Hash
+require 'logger'
+class LogEngine < Logger
 	
-	def initialize
-		super()
-		self[:log_level] = :info
-		self[:log_output] = STDERR
-		opts = OptionParser.new do |opts|
-			opts.on('-L','--log-level [STRING]','sets the Test Log Level') do |string|
-				self[:log_level] = string.to_sym
-			end
-			opts.on('-O','--log-output [I/O]','sets the Test Log output') do |io|
-				self[:log_output] = io
+	def LogEngine.setup
+		if $logger_args[:log_level].is_a?(Symbol)
+			$logger_args[:log_level] = case $logger_args[:log_level]
+			when :debug
+				DEBUG
+			when :info
+				INFO
+			when :warn
+				WARN
+			when :error
+				ERROR
+			when :fatal
+				FATAL
+			else
+				UNKNOWN
 			end
 		end
-		opts.parse!(@@argv)
+		log = self.new($logger_args[:log_output])
+		log.level = $logger_args[:log_level]
+		return log
 	end
 	
-	def self.keep_wanted_argv
-		args = Array.new(ARGV); ARGV.clear; keep = []
-		wanted = ['-L', '--log-level', '-O', '--log-output']
-		catch :finished do
-			loop do
-				throw :finished if args.empty?
-				catch :found do
-					wanted.each do |arg|
-						if args[0] == arg
-							2.times { keep << args.shift }
-							throw :found
-						end
-					end
-					ARGV << args.shift
+	class Arguments < Hash
+		require 'optparse'
+		
+		def initialize
+			super()
+			self[:log_level] = :info
+			self[:log_output] = STDERR
+			opts = OptionParser.new do |opts|
+				opts.on('-L','--log-level [STRING]','sets the Test Log Level') do |string|
+					self[:log_level] = string.to_sym
+				end
+				opts.on('-O','--log-output [I/O]','sets the Test Log output') do |io|
+					self[:log_output] = io
 				end
 			end
+			opts.parse!($logger_argv)
 		end
-		return keep
-	end
-	
-	@@argv = self.keep_wanted_argv
-	
-end
 
-module LogHelper
-	require 'logger'
-	
-	def setup_logger
-		args = Arguments.new
-		Abundance::log_level = args[:log_level]
-		@log_test = Logger.new(args[:log_output])
-		case args[:log_level]
-		when :debug
-			@log_test.level = Logger::DEBUG
-		when :info
-			@log_test.level = Logger::INFO
-		when :warn
-			@log_test.level = Logger::WARN
-		when :error
-			@log_test.level = Logger::ERROR
-		when :fatal
-			@log_test.level = Logger::FATAL
-		else
-			@log_test.level = Logger::UNKNOWN
+		def self.keep_wanted_argv
+			args = Array.new(ARGV); ARGV.clear; keep = []
+			wanted = ['-L', '--log-level', '-O', '--log-output']
+			catch :finished do
+				loop do
+					throw :finished if args.empty?
+					catch :found do
+						wanted.each do |arg|
+							if args[0] == arg
+								2.times { keep << args.shift }
+								throw :found
+							end
+						end
+						ARGV << args.shift
+					end
+				end
+			end
+			return keep
 		end
+
+		$logger_argv ||= self.keep_wanted_argv
+		$logger_args ||= self.new
+
 	end
 	
 end
